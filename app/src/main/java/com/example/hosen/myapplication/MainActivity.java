f04b404ea573bccceeff2dd40f24030f016d5c89
mohamd <mohamdib@gmail.com>
2017-07-23 15:39:17 +0300

    aa

--- app/src/main/java/com/example/hosen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hosen/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hosen.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import  android.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.security.acl.Group;
 
 public class MainActivity extends AppCompatActivity {
     Fragment frt;
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
          docsBt=(Button)findViewById(R.id.docsBt);
          votesBt=(Button)findViewById(R.id.votesBt);
          chatBt=(Button)findViewById(R.id.chatBt);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
         chatBt.setOnClickListener(new View.OnClickListener() {
               @Override
               public void onClick(View view) {
@@ -118,8 +123,10 @@ public class MainActivity extends AppCompatActivity {
         Log.i("fly","im pressed");
 
     }
+
     @Override
     public void onBackPressed() {
-
+        Intent i=new Intent(MainActivity.this, GroupListActivity.class);
+        startActivity(i);
     }
 }
